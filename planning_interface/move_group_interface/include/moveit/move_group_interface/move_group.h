fa52265f0fe02f007a7b8b282d6d0e11fea6441c
Martin Szarski <mszarski@gmail.com>
2014-02-09 13:21:42 +1100

    computeCartesianPath now accepts path constraints

--- planning_interface/move_group_interface/include/moveit/move_group_interface/move_group.h
+++ planning_interface/move_group_interface/include/moveit/move_group_interface/move_group.h
@@ -576,6 +576,17 @@ public:
   double computeCartesianPath(const std::vector<geometry_msgs::Pose> &waypoints, double eef_step, double jump_threshold,
                               moveit_msgs::RobotTrajectory &trajectory,  bool avoid_collisions = true);
 
+  /** \brief Compute a Cartesian path that follows specified waypoints with a step size of at most \e eef_step meters
+      between end effector configurations of consecutive points in the result \e trajectory. The reference frame for the
+      waypoints is that specified by setPoseReferenceFrame(). No more than \e jump_threshold
+      is allowed as change in distance in the configuration space of the robot (this is to prevent 'jumps' in IK solutions).
+      Kinematic constraints for the path given by \e path_constraints will be met for every point along the trajectory.
+      Constraints are checked (collision and kinematic) if \e avoid_collisions is set to true. If constraints cannot be met, the function fails.
+      Return a value that is between 0.0 and 1.0 indicating the fraction of the path achieved as described by the waypoints.
+      Return -1.0 in case of error. */
+  double computeCartesianPath(const std::vector<geometry_msgs::Pose> &waypoints, double step, double jump_threshold,
+                              moveit_msgs::RobotTrajectory &msg, bool avoid_collisions, moveit_msgs::Constraints &path_constraints);
+
   /** \brief Stop any trajectory execution, if one is active */
   void stop();
 
