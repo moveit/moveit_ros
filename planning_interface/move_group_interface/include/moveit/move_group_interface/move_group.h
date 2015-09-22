997d8ba51efc28c5065ae053add8a2c2af3460cc
dg <dg@shadowrobot.com>
2015-09-22 10:51:52 +0100

    added defs to header

--- planning_interface/move_group_interface/include/moveit/move_group_interface/move_group.h
+++ planning_interface/move_group_interface/include/moveit/move_group_interface/move_group.h
@@ -135,6 +135,10 @@ public:
   /** \brief Get the name of the frame in which the robot is planning */
   const std::string& getPlanningFrame() const;
 
+  const std::vector<std::string>& getJointNames();
+
+  std::vector<double> getNamedTargetValues(const std::string& name);
+
   /** \brief Get only the active (actuated) joints this instance operates on */
   const std::vector<std::string>& getActiveJoints() const;
 
